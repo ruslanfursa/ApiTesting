e112a10ccb8acbfbb27eb9c97c666cf766141aca
Ruslan Fursa <fursa_1986@mail.ru>
2023-08-28 16:54:01 +0300

    chech github hook

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -5,13 +5,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
 public class ReqresTest {
     private final static String URL = "https://reqres.in";
+
     @Test
-    public void testIfAvatarContainsId(){
+    public void testIfAvatarContainsId() {
         List<UserData> users = given()
                 .when()
                 .contentType(ContentType.JSON)
@@ -19,12 +21,14 @@ public class ReqresTest {
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
 //        users.forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
-        for(UserData us : users){
+        for (UserData us : users) {
             Assert.assertTrue(us.getAvatar().contains(us.getId().toString()));
         }
-    }
-    @Test
-    public void testTest(){
-        Assert.assertTrue(false);
+        List<String> avatars = users.stream().map(UserData::getAvatar).toList();
+        List<String> ids = users.stream().map(x -> x.getId().toString()).toList();
+
+        for(int i = 0; i < avatars.size(); i++){
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
     }
 }
