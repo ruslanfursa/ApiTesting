4baff133cff3f7fbeba6efea19ce05781cc9a0a9
Ruslan Fursa <fursa_1986@mail.ru>
2023-08-28 21:05:08 +0300

    add testSuccessRegistration

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static api.Specifications.*;
 import static io.restassured.RestAssured.given;
 
 public class ReqresTest {
@@ -14,21 +15,40 @@ public class ReqresTest {
 
     @Test
     public void testIfAvatarContainsId() {
+        Specifications.installSpecifications(Specifications.requestSpec(URL), Specifications.responseSpec(200));
         List<UserData> users = given()
                 .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "/api/users?page=2")
+//                .contentType(ContentType.JSON)
+                .get("/api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
 //        users.forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
-        for (UserData us : users) {
-            Assert.assertTrue(us.getAvatar().contains(us.getId().toString()));
-        }
+//        for (UserData us : users) {
+//            Assert.assertTrue(us.getAvatar().contains(us.getId().toString()));
+//        }
         List<String> avatars = users.stream().map(UserData::getAvatar).toList();
         List<String> ids = users.stream().map(x -> x.getId().toString()).toList();
 
-        for(int i = 0; i < avatars.size(); i++){
+        for (int i = 0; i < avatars.size(); i++) {
             Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
         }
+    }
+
+    @Test
+    public void testSuccessRegistration() {
+        Specifications.installSpecifications(Specifications.requestSpec(URL), Specifications.responseSpec(200));
+
+        Integer id = 4;
+        String token = "QpwL5tke4Pnpja7X4";
+
+        Registration newUser = new Registration("eve.holt@reqres.in", "pistol");
+        SuccessRegistration successRegistration = given()
+                .when()
+                .body(newUser)
+                .post("/api/register")
+                .then().log().all()
+                .extract().as(SuccessRegistration.class);
+        Assert.assertEquals(id, successRegistration.getId());
+        Assert.assertEquals(token, successRegistration.getToken());
     }
 }
---
+++ src/test/java/api/Specifications.java
@@ -0,0 +1,26 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpec(String url){
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+    public static ResponseSpecification responseSpec(int code){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(code)
+                .build();
+    }
+    public static void installSpecifications(RequestSpecification request, ResponseSpecification response){
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
+}
